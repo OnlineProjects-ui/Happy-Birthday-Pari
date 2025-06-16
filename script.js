fad901d60b4dc8e7010bd6781df0ac54fc814934
Riyansh Singh <priyangmanjarisingh1978@gmail.com>
2025-06-16 20:40:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,32 +4,5 @@ const video = document.getElementById("mainVideo");
 const playBtn = document.querySelector(".play-btn");
 const closeBtn = document.querySelector(".close");
 
-playBtn.addEventListener("click", () => {
-  modal.style.display = "flex";
-  video.currentTime = 0;
-  video.play();
-  video.setAttribute("controls", true);
-});
-
-closeBtn.addEventListener("click", () => {
-  modal.style.display = "none";
-  video.pause();
-  video.currentTime = 0;
-  video.removeAttribute("controls");
-});
-
-window.addEventListener("click", (event) => {
-  if (event.target === modal) {
-    modal.style.display = "none";
-    video.pause();
-    video.currentTime = 0;
-    video.removeAttribute("controls");
-  }
-});
-
-// Autoplay clips
-document.querySelectorAll(".clips video").forEach((clip) => {
-  clip.loop = true;
-  clip.muted = true;
-  clip.play();
-});
+// Open modal and play video
+playBtn.addEventListener("click", ()
